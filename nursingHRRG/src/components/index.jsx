1e64d9764e9bc9cb551263fea29c80ddb42fcee0
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-04-22 06:32:04 -0300

    Stage 1
    The following components are created:
    - App.jsx
    - App.css
    - Views:
    Home
    Updated
    Licenses
    Contact
    
    - Componentes:
    Navbar
    Footer
    ScrollTop

---
+++ nursingHRRG/src/components/index.jsx
@@ -0,0 +1,7 @@
+import Navbar from './navbar/navBar';
+import Footer from './footer/footer';
+
+export {
+    Navbar,
+    Footer
+}
---
+++ nursingHRRG/src/views/index.jsx
@@ -0,0 +1,13 @@
+import Landing from './landing/landing';
+import Home from './home/home';
+import Updates  from './updates/updates';
+import Licences from './licences/licences';
+import Contact from './contact/contact';
+
+export {
+    Landing,
+    Home,
+    Updates,
+    Licences,
+    Contact
+}
