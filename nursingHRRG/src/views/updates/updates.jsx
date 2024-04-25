4581edd40daf2d8b2a1b968a9cafddd2f21a76c7
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-04-25 15:51:15 -0300

    View Updates
    The following sections were created:
    - Banner Updates
    - Title
    - Updates

--- nursingHRRG/src/views/updates/updates.jsx
+++ nursingHRRG/src/views/updates/updates.jsx
@@ -1,32 +1,115 @@
 import React from "react";
+import bannerUpdates from '../../assets/bannerupdate.jpg';
+import bannerUpdatesMobile from '../../assets/bannerupdatemobile.jpg';
+import card1 from '../../assets/cardupdate1.png';
+import card2 from '../../assets/cardupdate2.png';
+import ScrollTop from '../../components/scrolltop/scrollTop';
+import './updates.css';
 
 const Updates = () => {
     return(
-        <div className="container">
-            <div className="row my-5 d-flex justity-content-center">
-                <div className="col">
-                    <h1>Texto de relleno</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quaerat qui laboriosam unde necessitatibus rerum numquam. Ratione, ipsum magnam dicta eveniet quidem tempore assumenda expedita totam odit sequi rerum nesciunt omnis enim. Animi quae eos, ex repellendus voluptas saepe ducimus esse perspiciatis sit expedita culpa quia accusamus similique consectetur necessitatibus.</p>
+        
+        <div className="container-fluid">
+
+                {/* Sección Banner Updates */}
+                <div className="container">
+                    <div className="row my-5 d-flex justify-content-center position-relative d-none d-md-flex">
+                        <div className="col">
+                            <div className="orange-rectangle"></div>
+                            <div className="blue-rectangle"></div>
+                            <div className="image-container-updates">
+                                <img src={bannerUpdates} className="img-fluid col-12" alt="" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row my-4 d-flex justify-content-center text-center d-md-none">
+                    <div className="col">
+                        <div className="image-container mt-3">
+                            <img src={bannerUpdatesMobile} className="img-fluid" alt="" />
+                            <div className="image-overlay"></div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="row my-5 d-flex justity-content-center">
-                <div className="col">
-                    <h1>Texto de relleno</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quaerat qui laboriosam unde necessitatibus rerum numquam. Ratione, ipsum magnam dicta eveniet quidem tempore assumenda expedita totam odit sequi rerum nesciunt omnis enim. Animi quae eos, ex repellendus voluptas saepe ducimus esse perspiciatis sit expedita culpa quia accusamus similique consectetur necessitatibus.</p>
+
                 </div>
-            </div>
-            <div className="row my-5 d-flex justity-content-center">
-                <div className="col">
-                    <h1>Texto de relleno</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quaerat qui laboriosam unde necessitatibus rerum numquam. Ratione, ipsum magnam dicta eveniet quidem tempore assumenda expedita totam odit sequi rerum nesciunt omnis enim. Animi quae eos, ex repellendus voluptas saepe ducimus esse perspiciatis sit expedita culpa quia accusamus similique consectetur necessitatibus.</p>
+            
+                {/* Sección Título */}
+                <div className="container">
+                    <div className="row my-3 d-flex justify-content-center text-center">
+                        <div className="col">
+                            <h3 className="fw-bold my-3">Formación continua</h3>
+                            <p>Puedes visualizar las capcitaciones que quieras realizar, para planificarte con anticipacion y poder  formar parte de la experiencia contribuir a tu formación profesional.</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="row my-5 d-flex justity-content-center">
-                <div className="col">
-                    <h1>Texto de relleno</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quaerat qui laboriosam unde necessitatibus rerum numquam. Ratione, ipsum magnam dicta eveniet quidem tempore assumenda expedita totam odit sequi rerum nesciunt omnis enim. Animi quae eos, ex repellendus voluptas saepe ducimus esse perspiciatis sit expedita culpa quia accusamus similique consectetur necessitatibus.</p>
+            
+                {/* Sección Capacitaciones */}
+                <div className="container">
+                    <div className="row my-5 d-flex justify-content-center">
+
+                        <div className="col-12">
+                            <h4 className="fw-bold text-muted my-3">Capacitaciones mes de Mayo</h4>
+                            <div className="row d-flex justify-content-around mx-auto">
+                                <div className="col-12 col-md-8 col-lg-5 my-3">
+                                    <div className="card">
+                                        <img src={card1} className="card-img-top" alt="..."/>
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-12 col-md-8 col-lg-5 my-3">
+                                    <div className="card">
+                                        <img src={card2} className="card-img-top" alt="..."/>
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12">
+                        <div className="text-end">
+                            <h4 className="fw-bold text-muted mt-5 my-3">Capacitaciones mes de Junio</h4>
+
+                        </div>
+                            <div className="row d-flex justify-content-around mx-auto">
+                                <div className="col-12 col-md-8 col-lg-5 my-3">
+                                    <div className="card">
+                                        <img src={card1} className="card-img-top" alt="..."/>
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-12 col-md-8 col-lg-5 my-3">
+                                    <div className="card">
+                                        <img src={card2} className="card-img-top" alt="..."/>
+                                        <div className="card-body">
+                                            <h5 className="card-title">Card title</h5>
+                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+            
+            
                 </div>
-            </div>
+            <ScrollTop/>
         </div>
     );
 };
