f02715720bd013d065c0038b80d88ea2355b9a43
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-05-08 15:00:01 -0300

    Landing, Navbar.
    Effects and styles were updated.

--- src/views/landing/landing.jsx
+++ src/views/landing/landing.jsx
@@ -1,36 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PageTransitionEvent from '../../components/pagetransition/pagetransition';
+import imgNurse from '../../assets/nurse.png';
+import logoLanding from '../../assets/logoLanding.png';
 import './landing.css';
 
 const Landing = () => {
     return(
-        <div className="container-landing">
-            <div className="row columna-izquierda">
-                <div className="col-md-6 ">
+        <PageTransitionEvent>
+            <div className="container-landing">
+                <div className="left-section">
+                    <img src={imgNurse} alt="" className="img-fluid img-custom-landing"/>
+                    <img src={logoLanding} alt="Logo" className="logo"/>
+                </div>
+                <div className="right-section">
                     <div className="text-center">
-                        <h1>View Landing</h1>
-                        <div className="mb-3">
-                            <Link to="/home">
-                                <button className="btn-custom-landing">Inicio</button>
-                            </Link>
-                        </div>
-                        <div className="mb-3">
-                            <Link to="/updates">
-                                <button className="btn-custom-landing">Capacitación</button>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link to="/licences">
-                                <button className="btn-custom-landing">Licencias</button>
-                            </Link>
+                        <h1 className="fw-bold">Bienvenido</h1>
+                        <div className="my-5">
+                            <div className="mb-3">
+                                <Link to="/home">
+                                    <button className="btn-custom-landing" >Inicio</button>
+                                </Link>
+                            </div>
+                            <div className="mb-3">
+                                <Link to="/updates">
+                                    <button className="btn-custom-landing" >Capacitación</button>
+                                </Link>
+                            </div>
+                            <div>
+                                <Link to="/licences">
+                                    <button className="btn-custom-landing" >Licencias</button>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-6">
-                    {/* Columna derecha vacía */}
-                </div>
             </div>
-        </div>
+        </PageTransitionEvent>
     );
 };
 
