a3634ccd37d17cea31d8c62a25b85e13474e20df
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-05-08 21:57:07 -0300

    Landing
    Content were created.
    Effects and styles were updated.

--- src/views/landing/landing.jsx
+++ src/views/landing/landing.jsx
@@ -3,18 +3,21 @@ import { Link } from "react-router-dom";
 import PageTransitionEvent from '../../components/pagetransition/pagetransition';
 import imgNurse from '../../assets/nurse.png';
 import logoLanding from '../../assets/logoLanding.png';
+import logoLanding2 from '../../assets/logoLanding2.png';
+import titleMobile from '../../assets/titleMobile.png';
 import './landing.css';
 
 const Landing = () => {
     return(
         <PageTransitionEvent>
             <div className="container-landing">
-                <div className="left-section">
+                <div className="left-section d-none d-md-block">
                     <img src={imgNurse} alt="" className="img-fluid img-custom-landing"/>
                     <img src={logoLanding} alt="Logo" className="logo"/>
                 </div>
                 <div className="right-section">
                     <div className="text-center">
+                        <img src={logoLanding2} alt="Logo" className="logoMobile"/>
                         <h1 className="fw-bold">Bienvenido</h1>
                         <div className="my-5">
                             <div className="mb-3">
@@ -33,6 +36,7 @@ const Landing = () => {
                                 </Link>
                             </div>
                         </div>
+                        <img src={titleMobile} alt="Logo" className="titleMobile"/>
                     </div>
                 </div>
             </div>
