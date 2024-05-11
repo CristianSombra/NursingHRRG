e086fc501d1ff1032a2052e9ebe4846eba5eb610
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-05-11 02:39:51 -0300

    Home
    FirstSectionNews component were created for Home.

---
+++ src/components/homeComponents/firstSectionNews.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import stand1 from '../../assets/stand1.png';
+
+
+const FirstSectionNews = () => {
+    return(
+        <div>
+            <img src={stand1} className="img-fluid img-custom-home my-3" alt="" />
+            <h5 className="fw-bold text-center text-md-start">Semana de presentación de stands de Enfermería</h5>
+            <p className="text-center text-md-start">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem itaque totam voluptate! Doloremque aliquam expedita earum quod? Libero, minus.</p>
+        </div>
+    )
+}
+
+export default FirstSectionNews;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,11 @@
 import Navbar from './navbar/navBar';
 import Footer from './footer/footer';
 import CarouselHome from './homeComponents/carouselHome';
+import FirstSectionNews from './homeComponents/firstSectionNews';
 
 export {
     Navbar,
     CarouselHome,
+    FirstSectionNews,
     Footer
 }
