72c4900974d9d7fe2170c5f9f0bac102dfcbeb7c
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-05-11 02:34:43 -0300

    carouselHome
    Component carousel home created.

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import { motion } from "framer-motion";
 import PageTransition from '../../components/pagetransition/pagetransition';
 import ScrollTop from '../../components/scrolltop/scrollTop';
+import { CarouselHome } from "../../components/index";
 import bannerHome from '../../assets/bannerhome.jpg';
 import bannerHomeMobile from '../../assets/bannerhomemobile.jpg';
-import img1 from '../../assets/img1.png';
-import img2 from '../../assets/img1.png';
-import img3 from '../../assets/img1.png';
 import stand1 from '../../assets/stand1.png';
 import card1 from '../../assets/card1.png'
 import card2 from '../../assets/card2.png'
@@ -128,32 +126,7 @@ const Home = () => {
                     </motion.div>
                     
                     <div className="col-12 col-md-10 col-lg-8 my-3 mx-auto">
-                        <div id="carouselIndicators" className="carousel slide" data-bs-ride="carousel">
-                            <div className="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                            </div>
-                            <div className="carousel-inner">
-                                    <div className="carousel-item active">
-                                        <img src={img1} className="d-block w-100" alt="" />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img src={img2} className="d-block w-100" alt="" />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img src={img3} className="d-block w-100" alt="" />
-                                    </div>
-                            </div>
-                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Previous</span>
-                            </button>
-                            <button className="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span className="visually-hidden">Next</span>
-                            </button>
-                        </div>
+                        <CarouselHome/>
                     </div>
 
                     <div className="col-12">
