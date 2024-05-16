79702c645625da8d3e63b77516f1da340a85a869
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-05-16 01:19:25 -0300

    Spinner Loader
    Component created to improve the user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { Navbar, Footer } from './components/index';
+import { Navbar, Footer, SpinnerLoader } from './components/index';
 import {
   Landing,
   Home,
@@ -18,20 +18,32 @@ function App() {
   const location = useLocation();
   const showNav = location.pathname !== "/";
   const showFooter = location.pathname !== "/";
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000)
+    return () => clearTimeout(timer)
+  }, []);
 
 
   return (
     <div>
-      {showNav && <Navbar/>}
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/home" element={<Home />} />
-        <Route exact path="/updates" element={<Updates />} />
-        <Route exact path="/licences" element={<Licenses />} />
-        <Route exact path="/contact" element={<Contact />} />
-      </Routes>
-      <ToastContainer />
-      {showFooter && <Footer />}
+      {loading ? <SpinnerLoader /> : (
+        <>
+          {showNav && <Navbar />}
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/updates" element={<Updates />} />
+            <Route exact path="/licences" element={<Licenses />} />
+            <Route exact path="/contact" element={<Contact />} />
+          </Routes>
+          <ToastContainer />
+          {showFooter && <Footer />}
+        </>
+      )}
     </div>
   );
 };
