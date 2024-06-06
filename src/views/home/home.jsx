535ea07515b7662df3cfc552d028fa2791715845
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-06-06 10:22:34 -0300

    Inscription updated.

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -11,7 +11,7 @@ import '@/components/homeComponents/home.css';
 const Home = () => {
 
     const handleButtonClick = () => {
-        window.open('https://forms.gle/EHrgZpx45cYZ8gSAA', '_blank');
+        window.open('https://forms.gle/HMX5hbQP5ZaDMZDEA', '_blank');
     };
 
     return(
